2267e49a742259558057b9969d706475098120c8
Ghazanfar <ghazanfar.isc@gmail.com>
2023-01-08 23:20:50 +0530

    Add support for v3 APIs

--- alerting/notifier/channels/kafka.go
+++ alerting/notifier/channels/kafka.go
@@ -32,6 +32,11 @@ type kafkaRecord struct {
 	IncidentKey string         `json:"incident_key,omitempty"`
 }
 
+type kafkaV3Record struct {
+	Type string      `json:"type"`
+	Data kafkaRecord `json:"data"`
+}
+
 type kafkaContext struct {
 	Type   string `json:"type"`
 	Source string `json:"src"`
@@ -49,15 +54,35 @@ type KafkaNotifier struct {
 }
 
 type kafkaSettings struct {
-	Endpoint     string `json:"kafkaRestProxy,omitempty" yaml:"kafkaRestProxy,omitempty"`
-	Topic        string `json:"kafkaTopic,omitempty" yaml:"kafkaTopic,omitempty"`
-	Description  string `json:"description,omitempty" yaml:"description,omitempty"`
-	Details      string `json:"details,omitempty" yaml:"details,omitempty"`
-	BasicAuth    bool   `json:"basicAuth,omitempty" yaml:"basicAuth,omitempty"`
-	PassFilePath string `json:"passwordFilePath,omitempty" yaml:"passwordFilePath,omitempty"`
-	AuthUser     string `json:"user,omitempty" yaml:"user,omitempty"`
-	AuthPass     string `json:"password,omitempty" yaml:"password,omitempty"`
+	Endpoint       string `json:"kafkaRestProxy,omitempty" yaml:"kafkaRestProxy,omitempty"`
+	Topic          string `json:"kafkaTopic,omitempty" yaml:"kafkaTopic,omitempty"`
+	Description    string `json:"description,omitempty" yaml:"description,omitempty"`
+	Details        string `json:"details,omitempty" yaml:"details,omitempty"`
+	BasicAuth      bool   `json:"basicAuth,omitempty" yaml:"basicAuth,omitempty"`
+	BasicAuthUser  string `json:"basicAuthUser,omitempty" yaml:"user,omitempty"`
+	PasswordSource string `json:"passwordSource,omitempty" yaml:"passwordSource,omitempty"`
+	PassFilePath   string `json:"passwordFilePath,omitempty" yaml:"passwordFilePath,omitempty"`
+	AuthPass       string `json:"password,omitempty" yaml:"password,omitempty"`
+	APIVersion     string `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty"`
+	KafkaClusterID string `json:"kafkaClusterId,omitempty" yaml:"kafkaClusterId,omitempty"`
 }
+
+// The user can either enter the password in the Grafana UI
+// or provide a file path from which to read the password.
+// These values represent the two options.
+const (
+	PasswordSourceFromInput = "password"
+	PasswordSourceFromFile  = "passwordFile"
+)
+
+// The user can choose which API version to use when sending
+// messages to Kafka. The default is v2.
+// Details on how these versions differ can be found here:
+// https://docs.confluent.io/platform/current/kafka-rest/api.html
+const (
+	APIVersionV2 = "v2"
+	APIVersionV3 = "v3"
+)
 
 func buildKafkaSettings(fc FactoryConfig) (*kafkaSettings, error) {
 	var settings kafkaSettings
@@ -78,15 +103,34 @@ func buildKafkaSettings(fc FactoryConfig) (*kafkaSettings, error) {
 	if settings.Details == "" {
 		settings.Details = DefaultMessageEmbed
 	}
+
+	if settings.APIVersion == "" {
+		settings.APIVersion = APIVersionV2
+	} else if settings.APIVersion == APIVersionV3 {
+		if settings.KafkaClusterID == "" {
+			return nil, errors.New("kafka cluster id must be provided when using api version 3")
+		}
+	} else if settings.APIVersion != APIVersionV2 && settings.APIVersion != APIVersionV3 {
+		return nil, fmt.Errorf("unsupported api version: %s", settings.APIVersion)
+	}
+
 	if settings.BasicAuth {
-		if settings.AuthUser == "" {
+		if settings.BasicAuthUser == "" {
 			return nil, errors.New("if basic auth is enabled, user must be provided")
 		}
-		if settings.PassFilePath == "" {
-			return nil, errors.New("if basic auth is enabled, password file path must be provided")
-		}
-		if err := settings.RefreshPassword(); err != nil {
-			return nil, err
+		if settings.PasswordSource == PasswordSourceFromFile {
+			if settings.PassFilePath == "" {
+				return nil, errors.New("password file path must be provided")
+			}
+			if err := settings.RefreshPassword(); err != nil {
+				return nil, err
+			}
+		} else if settings.PasswordSource == PasswordSourceFromInput {
+			if settings.AuthPass == "" {
+				return nil, errors.New("password must be provided")
+			}
+		} else {
+			return nil, fmt.Errorf("invalid password source: %s", settings.PasswordSource)
 		}
 	}
 	return &settings, nil
@@ -131,6 +175,14 @@ func newKafkaNotifier(fc FactoryConfig) (*KafkaNotifier, error) {
 
 // Notify sends the alert notification.
 func (kn *KafkaNotifier) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
+	if kn.settings.APIVersion == APIVersionV3 {
+		return kn.notifyWithAPIV3(ctx, as...)
+	}
+	return kn.notifyWithAPIV2(ctx, as...)
+}
+
+// Use the v2 API to send the alert notification.
+func (kn *KafkaNotifier) notifyWithAPIV2(ctx context.Context, as ...*types.Alert) (bool, error) {
 	var tmplErr error
 	tmpl, _ := TmplText(ctx, kn.tmpl, as, kn.log, &tmplErr)
 
@@ -157,20 +209,99 @@ func (kn *KafkaNotifier) Notify(ctx context.Context, as ...*types.Alert) (bool, 
 
 	retryOnFailure := false
 	if kn.settings.BasicAuth {
-		cmd.User = kn.settings.AuthUser
+		cmd.User = kn.settings.BasicAuthUser
 		cmd.Password = kn.settings.AuthPass
-		retryOnFailure = true
+
+		// Only retry if the password is read from a file becase the password could have changed.
+		retryOnFailure = kn.settings.PasswordSource == PasswordSourceFromFile
 	}
 
 	return kn.sendWebhookWithRetry(ctx, cmd, retryOnFailure)
 }
 
+// Use the v3 API to send the alert notification.
+func (kn *KafkaNotifier) notifyWithAPIV3(ctx context.Context, as ...*types.Alert) (bool, error) {
+	var tmplErr error
+	tmpl, _ := TmplText(ctx, kn.tmpl, as, kn.log, &tmplErr)
+
+	// For v3 the Produce URL is like this,
+	// <Endpoint>/kafka/v3/clusters/<Cluster ID>/topics/<Topic Name>/records
+	topicURL := strings.TrimRight(kn.settings.Endpoint, "/") + "/kafka/v3/clusters/" + tmpl(kn.settings.KafkaClusterID) + "/topics/" + tmpl(kn.settings.Topic) + "/records"
+
+	body, err := kn.buildV3Body(ctx, tmpl, as...)
+	if err != nil {
+		return false, err
+	}
+	if tmplErr != nil {
+		kn.log.Warn("failed to template Kafka message", "error", tmplErr.Error())
+	}
+
+	cmd := &SendWebhookSettings{
+		URL:        topicURL,
+		Body:       body,
+		HTTPMethod: "POST",
+		HTTPHeader: map[string]string{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		},
+		Validation: validateKafkaV3Response,
+	}
+
+	retryOnFailure := false
+	if kn.settings.BasicAuth {
+		cmd.User = kn.settings.BasicAuthUser
+		cmd.Password = kn.settings.AuthPass
+
+		// Only retry if the password is read from a file becase the password could have changed.
+		retryOnFailure = kn.settings.PasswordSource == PasswordSourceFromFile
+	}
+
+	// TODO: Convert to a stream - keep a single connection open and send records on it.
+	// Can be implemented nicely using channels. The v3 API can be used in streaming mode
+	// by setting “Transfer-Encoding: chunked” header.
+	// For as long as the connection is kept open, the server will keep accepting records.
+	return kn.sendWebhookWithRetry(ctx, cmd, retryOnFailure)
+}
+
+/*
+A sample of V3 response looks like this,
+
+	{
+		"error_code": 200,
+		"cluster_id": "lkc-abcd",
+		"topic_name": "myTopic",
+		"partition_id": 5,
+		"offset": 0,
+		"timestamp": "2023-01-08T11:21:48.031Z",
+		"value": { "type" : "JSON", "size" : 14 }
+	}
+*/
+type kafkaV3Response struct {
+	ErrorCode int `json:"error_code"`
+}
+
+func validateKafkaV3Response(rawResponse []byte, statusCode int) error {
+	if statusCode/100 != 2 {
+		return fmt.Errorf("unexpected status code %d", statusCode)
+	}
+	// 200 status means the API was processed successfully.
+	// The message publishing could still fail. This is verified by checking the error_code field in the response.
+	var response kafkaV3Response
+	if err := json.Unmarshal(rawResponse, &response); err != nil {
+		return err
+	}
+	if response.ErrorCode/100 != 2 {
+		return fmt.Errorf("failed to publish message to Kafka. response: %s", string(rawResponse))
+	}
+	return nil
+}
+
 func (kn *KafkaNotifier) sendWebhookWithRetry(ctx context.Context, cmd *SendWebhookSettings, retry bool) (bool, error) {
 	if err := kn.ns.SendWebhook(ctx, cmd); err != nil {
 		kn.log.Error("Failed to send notification to Kafka", "error", err, "body", cmd.Body)
-		// No way to check if this was an auth error. Retry with a refreshed password
+		// No clean way to check if this was an auth error. Retry with a refreshed password
 		if retry {
-			kn.log.Debug("Retrying with a refreshed password")
+			kn.log.Debug("Retrying with a refreshed password.")
 			if err := kn.settings.RefreshPassword(); err != nil {
 				return false, err
 			}
@@ -186,7 +317,48 @@ func (kn *KafkaNotifier) SendResolved() bool {
 }
 
 func (kn *KafkaNotifier) buildBody(ctx context.Context, tmpl func(string) string, as ...*types.Alert) (string, error) {
+	if kn.settings.APIVersion == APIVersionV3 {
+		return kn.buildV3Body(ctx, tmpl, as...)
+	}
+	return kn.buildV2Body(ctx, tmpl, as...)
+}
+
+func (kn *KafkaNotifier) buildV2Body(ctx context.Context, tmpl func(string) string, as ...*types.Alert) (string, error) {
 	var record kafkaRecord
+	if err := kn.buildKafkaRecord(ctx, &record, tmpl, as...); err != nil {
+		return "", err
+	}
+	records := kafkaBody{
+		Records: []kafkaRecordEnvelope{
+			{Value: record},
+		},
+	}
+	body, err := json.Marshal(records)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (kn *KafkaNotifier) buildV3Body(ctx context.Context, tmpl func(string) string, as ...*types.Alert) (string, error) {
+	var record kafkaRecord
+	if err := kn.buildKafkaRecord(ctx, &record, tmpl, as...); err != nil {
+		return "", err
+	}
+	records := map[string]kafkaV3Record{
+		"value": {
+			Type: "JSON",
+			Data: record,
+		},
+	}
+	body, err := json.Marshal(records)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func (kn *KafkaNotifier) buildKafkaRecord(ctx context.Context, record *kafkaRecord, tmpl func(string) string, as ...*types.Alert) error {
 	record.Client = "Grafana"
 	record.Description = tmpl(kn.settings.Description)
 	record.Details = tmpl(kn.settings.Details)
@@ -205,21 +377,10 @@ func (kn *KafkaNotifier) buildBody(ctx context.Context, tmpl func(string) string
 
 	groupKey, err := notify.ExtractGroupKey(ctx)
 	if err != nil {
-		return "", err
+		return err
 	}
 	record.IncidentKey = groupKey.Hash()
-
-	records := kafkaBody{
-		Records: []kafkaRecordEnvelope{
-			{Value: record},
-		},
-	}
-
-	body, err := json.Marshal(records)
-	if err != nil {
-		return "", err
-	}
-	return string(body), nil
+	return nil
 }
 
 func buildState(as ...*types.Alert) AlertStateType {
