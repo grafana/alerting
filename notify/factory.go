c9f0b987e3c65571babaa5908b6cc06d99823f57
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-02-14 14:54:02 -0500

    replace factory with BuildReceiverIntegrations

--- notify/factory.go
+++ notify/factory.go
@@ -1,10 +1,11 @@
 package notify
 
 import (
-	"strings"
-
 	"github.com/prometheus/alertmanager/notify"
+	"github.com/prometheus/alertmanager/template"
 
+	"github.com/grafana/alerting/images"
+	"github.com/grafana/alerting/logging"
 	"github.com/grafana/alerting/receivers"
 	"github.com/grafana/alerting/receivers/alertmanager"
 	"github.com/grafana/alerting/receivers/dinding"
@@ -27,57 +28,125 @@ import (
 	"github.com/grafana/alerting/receivers/wecom"
 )
 
-var receiverFactories = map[string]func(receivers.FactoryConfig) (NotificationChannel, error){
-	"prometheus-alertmanager": wrap(alertmanager.New),
-	"dingding":                wrap(dinding.New),
-	"discord":                 wrap(discord.New),
-	"email":                   wrap(email.New),
-	"googlechat":              wrap(googlechat.New),
-	"kafka":                   wrap(kafka.New),
-	"line":                    wrap(line.New),
-	"opsgenie":                wrap(opsgenie.New),
-	"pagerduty":               wrap(pagerduty.New),
-	"pushover":                wrap(pushover.New),
-	"sensugo":                 wrap(sensugo.New),
-	"slack":                   wrap(slack.New),
-	"teams":                   wrap(teams.New),
-	"telegram":                wrap(telegram.New),
-	"threema":                 wrap(threema.New),
-	"victorops":               wrap(victorops.New),
-	"webhook":                 wrap(webhook.New),
-	"wecom":                   wrap(wecom.New),
-	"webex":                   wrap(webex.New),
-}
-
 type NotificationChannel interface {
 	notify.Notifier
 	notify.ResolvedSender
 }
 
-func Factory(receiverType string) (func(receivers.FactoryConfig) (NotificationChannel, error), bool) {
-	receiverType = strings.ToLower(receiverType)
-	factory, exists := receiverFactories[receiverType]
-	return factory, exists
-}
+// BuildReceiverIntegrations builds notifiers of the receiver and wraps each of them in Integration.
+func BuildReceiverIntegrations(
+	receiver GrafanaReceiverConfig,
+	tmpl *template.Template,
+	ns receivers.NotificationSender,
+	img images.ImageStore, // Used by some receivers to include as part of the source
+	newLogger logging.LoggerFactory,
+	orgID int64,
+	version string,
+) []*Integration {
+	var integrations []*Integration
 
-// wrap wraps the notifier's factory errors with receivers.ReceiverValidationError
-func wrap[T NotificationChannel](f func(fc receivers.FactoryConfig) (T, error)) func(receivers.FactoryConfig) (NotificationChannel, error) {
-	return func(fc receivers.FactoryConfig) (NotificationChannel, error) {
-		ch, err := f(fc)
-		if err != nil {
-			return nil, ReceiverValidationError{
-				Err: err,
-				// TODO it will be removed in the next PR
-				Cfg: &GrafanaReceiver{
-					UID:                   fc.Config.UID,
-					Name:                  fc.Config.Name,
-					Type:                  fc.Config.Type,
-					DisableResolveMessage: fc.Config.DisableResolveMessage,
-					Settings:              fc.Config.Settings,
-					SecureSettings:        nil,
-				},
-			}
-		}
-		return ch, nil
+	createIntegration := func(idx int, cfg receivers.Metadata, f func(logger logging.Logger) NotificationChannel) {
+		logger := newLogger("ngalert.notifier." + cfg.Type) // TODO add more context here
+		n := f(logger)
+		i := NewIntegration(n, n, cfg.Type, idx)
+		integrations = append(integrations, i)
 	}
+
+	for i, cfg := range receiver.AlertmanagerConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return alertmanager.New2(cfg.Settings, cfg.Metadata, img, l)
+		})
+	}
+	for i, cfg := range receiver.DingdingConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return dinding.New2(cfg.Settings, cfg.Metadata, tmpl, ns, l)
+		})
+	}
+	for i, cfg := range receiver.DiscordConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return discord.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l, version)
+		})
+	}
+	for i, cfg := range receiver.EmailConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return email.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.GooglechatConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return googlechat.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l, version)
+		})
+	}
+	for i, cfg := range receiver.KafkaConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return kafka.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.LineConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return line.New2(cfg.Settings, cfg.Metadata, tmpl, ns, l)
+		})
+	}
+	for i, cfg := range receiver.OpsgenieConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return opsgenie.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.PagerdutyConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return pagerduty.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.PushoverConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return pushover.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.SensugoConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return sensugo.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.SlackConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return slack.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l, version)
+		})
+	}
+	for i, cfg := range receiver.TeamsConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return teams.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.TelegramConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return telegram.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.ThreemaConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return threema.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.VictoropsConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return victorops.New2(cfg.Settings, cfg.Metadata, tmpl, ns, img, l, version)
+		})
+	}
+	for i, cfg := range receiver.WebhookConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return webhook.New2(cfg.Settings, orgID, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+	for i, cfg := range receiver.WecomConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return wecom.New2(cfg.Settings, cfg.Metadata, tmpl, ns, l)
+		})
+	}
+	for i, cfg := range receiver.WebexConfigs {
+		createIntegration(i, cfg.Metadata, func(l logging.Logger) NotificationChannel {
+			return webex.New2(cfg.Settings, orgID, cfg.Metadata, tmpl, ns, img, l)
+		})
+	}
+
+	return integrations
 }
