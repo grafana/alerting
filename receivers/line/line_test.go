a3f5efb3eb46fcec5a16860f0c6f4bd724ad52bb
Jueun Lee <seirios0107@gmail.com>
2024-05-16 22:37:13 +0900

    Drop rule URL from LINE notifier message body  (#182)
    * drop rule url from line message body
    * check truncated message body when too long

--- receivers/line/line_test.go
+++ receivers/line/line_test.go
@@ -2,7 +2,9 @@ package line
 
 import (
 	"context"
+	"math/rand"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/prometheus/alertmanager/notify"
@@ -50,7 +52,7 @@ func TestNotify(t *testing.T) {
 				"Authorization": "Bearer sometoken",
 				"Content-Type":  "application/x-www-form-urlencoded;charset=UTF-8",
 			},
-			expMsg:      "message=%5BFIRING%3A1%5D++%28val1%29%0Ahttp%3A%2Flocalhost%2Falerting%2Flist%0A%0A%2A%2AFiring%2A%2A%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val1%0AAnnotations%3A%0A+-+ann1+%3D+annv1%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval1%0ADashboard%3A+http%3A%2F%2Flocalhost%2Fd%2Fabcd%0APanel%3A+http%3A%2F%2Flocalhost%2Fd%2Fabcd%3FviewPanel%3Defgh%0A",
+			expMsg:      "message=%5BFIRING%3A1%5D++%28val1%29%0A%2A%2AFiring%2A%2A%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val1%0AAnnotations%3A%0A+-+ann1+%3D+annv1%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval1%0ADashboard%3A+http%3A%2F%2Flocalhost%2Fd%2Fabcd%0APanel%3A+http%3A%2F%2Flocalhost%2Fd%2Fabcd%3FviewPanel%3Defgh%0A",
 			expMsgError: nil,
 		}, {
 			name: "Multiple alerts",
@@ -76,7 +78,7 @@ func TestNotify(t *testing.T) {
 				"Authorization": "Bearer sometoken",
 				"Content-Type":  "application/x-www-form-urlencoded;charset=UTF-8",
 			},
-			expMsg:      "message=%5BFIRING%3A2%5D++%0Ahttp%3A%2Flocalhost%2Falerting%2Flist%0A%0A%2A%2AFiring%2A%2A%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val1%0AAnnotations%3A%0A+-+ann1+%3D+annv1%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval1%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val2%0AAnnotations%3A%0A+-+ann1+%3D+annv2%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval2%0A",
+			expMsg:      "message=%5BFIRING%3A2%5D++%0A%2A%2AFiring%2A%2A%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val1%0AAnnotations%3A%0A+-+ann1+%3D+annv1%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval1%0A%0AValue%3A+%5Bno+value%5D%0ALabels%3A%0A+-+alertname+%3D+alert1%0A+-+lbl1+%3D+val2%0AAnnotations%3A%0A+-+ann1+%3D+annv2%0ASilence%3A+http%3A%2F%2Flocalhost%2Falerting%2Fsilence%2Fnew%3Falertmanager%3Dgrafana%26matcher%3Dalertname%253Dalert1%26matcher%3Dlbl1%253Dval2%0A",
 			expMsgError: nil,
 		}, {
 			name: "One alert custom title and description",
@@ -97,7 +99,7 @@ func TestNotify(t *testing.T) {
 				"Authorization": "Bearer sometoken",
 				"Content-Type":  "application/x-www-form-urlencoded;charset=UTF-8",
 			},
-			expMsg:      "message=customTitle+1%0Ahttp%3A%2Flocalhost%2Falerting%2Flist%0A%0AcustomDescription",
+			expMsg:      "message=customTitle+1%0AcustomDescription",
 			expMsgError: nil,
 		},
 	}
@@ -133,6 +135,84 @@ func TestNotify(t *testing.T) {
 
 			require.Equal(t, c.expHeaders, webhookSender.Webhook.HTTPHeader)
 			require.Equal(t, c.expMsg, webhookSender.Webhook.Body)
+		})
+	}
+}
+
+func TestTruncatedNotify(t *testing.T) {
+	tmpl := templates.ForTests(t)
+
+	externalURL, err := url.Parse("http://localhost")
+	require.NoError(t, err)
+	tmpl.ExternalURL = externalURL
+	title := "[FIRING:1]  (val1)\n"
+	cases := []struct {
+		name         string
+		settings     Config
+		alerts       []*types.Alert
+		expHeaders   map[string]string
+		expMsg       string
+		expInitError string
+		expMsgError  error
+	}{
+		{
+			name: "Truncated alert when message length is over 1000",
+			settings: Config{
+				Title:       templates.DefaultMessageTitleEmbed,
+				Description: strings.Repeat("Y", lineMaxMessageLenRunes+rand.Intn(1000)+1),
+				Token:       "sometoken",
+			},
+			alerts: []*types.Alert{
+				{
+					Alert: model.Alert{
+						Labels:      model.LabelSet{"alertname": "alert1", "lbl1": "val1"},
+						Annotations: model.LabelSet{"ann1": "annv1", "__dashboardUid__": "abcd", "__panelId__": "efgh"},
+					},
+				},
+			},
+			expHeaders: map[string]string{
+				"Authorization": "Bearer sometoken",
+				"Content-Type":  "application/x-www-form-urlencoded;charset=UTF-8",
+			},
+			expMsg:      title + strings.Repeat("Y", lineMaxMessageLenRunes-len(title)-1) + "…",
+			expMsgError: nil,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			webhookSender := receivers.MockNotificationService()
+
+			pn := &Notifier{
+				Base: &receivers.Base{
+					Name:                  "",
+					Type:                  "",
+					UID:                   "",
+					DisableResolveMessage: false,
+				},
+				log:      &logging.FakeLogger{},
+				ns:       webhookSender,
+				tmpl:     tmpl,
+				settings: c.settings,
+			}
+
+			ctx := notify.WithGroupKey(context.Background(), "alertname")
+			ctx = notify.WithGroupLabels(ctx, model.LabelSet{"alertname": ""})
+			ok, err := pn.Notify(ctx, c.alerts...)
+			if c.expMsgError != nil {
+				require.False(t, ok)
+				require.Error(t, err)
+				require.Equal(t, c.expMsgError.Error(), err.Error())
+				return
+			}
+			require.NoError(t, err)
+			require.True(t, ok)
+
+			require.Equal(t, c.expHeaders, webhookSender.Webhook.HTTPHeader)
+			form := url.Values{}
+			form.Add("message", c.expMsg)
+			expBody := form.Encode()
+			require.Equal(t, expBody, webhookSender.Webhook.Body)
+			require.Len(t, webhookSender.Webhook.Body, lineMaxMessageLenRunes+len(title)+len("message=")+1)
 		})
 	}
 }
