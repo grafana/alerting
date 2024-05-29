228d90891dee77799808f46f821fa1ac6c02ed47
Santiago <santiagohernandez.1997@gmail.com>
2024-05-29 15:38:35 +0200

    Parse config in a way that is compatible with the Mimir Alertmanager

--- definition/compat_test.go
+++ definition/compat_test.go
@@ -2,6 +2,7 @@ package definition
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/prometheus/alertmanager/config"
@@ -9,6 +10,114 @@ import (
 )
 
 var validConfig = []byte(`{"route":{"receiver":"grafana-default-email","routes":[{"receiver":"grafana-default-email","object_matchers":[["a","=","b"]],"mute_time_intervals":["test1"]}]},"mute_time_intervals":[{"name":"test1","time_intervals":[{"times":[{"start_time":"00:00","end_time":"12:00"}]}]}],"templates":null,"receivers":[{"name":"grafana-default-email","grafana_managed_receiver_configs":[{"uid":"uxwfZvtnz","name":"email receiver","type":"email","disableResolveMessage":false,"settings":{"addresses":"<example@email.com>"},"secureFields":{}}]}]}`)
+
+func TestLoadCompat(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []byte
+		expErr string
+	}{
+		{
+			name:   "no configuration",
+			input:  []byte(``),
+			expErr: "empty input",
+		},
+		{
+			name:   "no routes",
+			input:  []byte(`{}`),
+			expErr: "no routes provided",
+		},
+		{
+			name:   "duplicated receivers",
+			input:  []byte(testConfigDuplicatedReceivers),
+			expErr: "notification config name \"test\" is not unique",
+		},
+		{
+			name:  "no global config",
+			input: []byte(testConfigWithoutGlobal),
+		},
+		{
+			name:   "no slack api url",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "slack_configs")),
+			expErr: "no global Slack API URL set",
+		},
+		{
+			name:   "no Opsgenie api key",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "opsgenie_configs")),
+			expErr: "no global OpsGenie API Key set",
+		},
+		{
+			name:   "no WeChat api secret",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "wechat_configs")),
+			expErr: "no global Wechat ApiSecret set",
+		},
+		{
+			name:   "no VictorOps api key",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "victorops_configs")),
+			expErr: "no global VictorOps API Key set",
+		},
+		{
+			name:   "no Discord url",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "discord_configs")),
+			expErr: "no discord webhook URL provided",
+		},
+		{
+			name:   "no MSTeams url",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "msteams_configs")),
+			expErr: "no msteams webhook URL provided",
+		},
+		{
+			name:   "no smarthost",
+			input:  []byte(fmt.Sprintf(missingValuesTemplate, "email_configs")),
+			expErr: "no global SMTP smarthost set",
+		},
+		{
+			name:  "with global config",
+			input: []byte(testConfigWithGlobal),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c, err := LoadCompat([]byte(test.input))
+			if test.expErr != "" {
+				require.Error(t, err)
+				require.Equal(t, test.expErr, err.Error())
+				return
+			}
+
+			require.NoError(t, err)
+
+			// It should add the default global config.
+			require.NotNil(t, c.Global)
+			globalConfig := c.Global
+
+			// All configs should have the default http config set except for Webex.
+			require.Equal(t, c.Receivers[0].DiscordConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].MSTeamsConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].OpsGenieConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].PagerdutyConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].PushoverConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].SNSConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].SlackConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].TelegramConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].VictorOpsConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].WebhookConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+			require.Equal(t, c.Receivers[0].WechatConfigs[0].HTTPConfig, globalConfig.HTTPConfig)
+
+			if len(c.Receivers[0].EmailConfigs) > 0 {
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].Smarthost, globalConfig.SMTPSmarthost)
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].From, globalConfig.SMTPFrom)
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].AuthUsername, globalConfig.SMTPAuthUsername)
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].AuthPassword, globalConfig.SMTPAuthPassword)
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].AuthSecret, globalConfig.SMTPAuthSecret)
+				require.Equal(t, c.Receivers[0].EmailConfigs[0].AuthIdentity, globalConfig.SMTPAuthIdentity)
+				require.Equal(t, *c.Receivers[0].EmailConfigs[0].RequireTLS, globalConfig.SMTPRequireTLS)
+			}
+		})
+	}
+
+}
 
 func TestGrafanaToUpstreamConfig(t *testing.T) {
 	cfg, err := Load(validConfig)
@@ -56,3 +165,132 @@ func TestPostableApiReceiverToApiReceiver(t *testing.T) {
 	require.Equal(t, json.RawMessage{'b', 'y', 't', 'e', 's'}, i.Settings)
 	require.Equal(t, map[string]string{"key": "value"}, i.SecureSettings)
 }
+
+const testConfigWithoutGlobal = `
+route:
+  receiver: test
+  routes:
+    - receiver: test
+receivers:
+  - name: test
+    discord_configs:
+      - webhook_url: http://test.com
+    msteams_configs:
+      - webhook_url: http://test.com
+    opsgenie_configs:
+      - api_key: test
+    pagerduty_configs:
+      - routing_key: test
+    pushover_configs:
+      - user_key: test
+        token: test
+    slack_configs:
+      - api_url: http://test.com
+    sns_configs:
+      - topic_arn: test
+    telegram_configs:
+      - bot_token: test
+        chat_id: 1
+    victorops_configs:
+      - api_key: test
+        routing_key: test
+    webhook_configs:
+      - url: http://test.com
+    wechat_configs:
+      - api_key: test
+        api_secret: test
+        corp_id: test
+    webex_configs:
+      - api_url: http://test.com
+        room_id: test
+        http_config:
+          bearer_token: test
+`
+
+const testConfigWithGlobal = `
+global:
+  smtp_smarthost: smtp.example.org:587
+  smtp_from: testfrom@test.com
+  resolve_timeout: 5m
+  http_config:
+    follow_redirects: false
+    enable_http2: false
+  smtp_hello: test
+  smtp_require_tls: false
+  pagerduty_url: https://pagerdutytest.com
+  slack_api_url: https://slacktest.com
+  opsgenie_api_url: https://opsgenietest.com
+  opsgenie_api_key: test
+  wechat_api_url: https://wechattest.com
+  wechat_api_secret: test
+  wechat_api_corp_id: test_id
+  victorops_api_url: https://victoropstest.com
+  victorops_api_key: test
+  telegram_api_url: https://telegramtest.com
+  webex_api_url: https://webextest.com
+route:
+  receiver: test
+  routes:
+    - receiver: test
+receivers:
+  - name: test
+    email_configs:
+      - to: test
+    discord_configs:
+      - webhook_url: http://test.com
+    msteams_configs:
+      - webhook_url: http://test.com
+    opsgenie_configs:
+      - send_resolved: true
+    pagerduty_configs:
+      - routing_key: test
+    pushover_configs:
+      - user_key: test
+        token: test
+    slack_configs:
+      - channel: test
+    sns_configs:
+      - topic_arn: test
+    telegram_configs:
+      - bot_token: test
+        chat_id: 1
+    victorops_configs:
+      - routing_key: test
+    webhook_configs:
+      - url: http://test.com
+    wechat_configs:
+      - api_key: test
+    webex_configs:
+      - room_id: test
+        http_config:
+          bearer_token: test
+`
+
+const testConfigDuplicatedReceivers = `
+route:
+  receiver: test
+  routes:
+    - receiver: test
+receivers:
+  - name: test
+  - name: test
+`
+
+const missingValuesTemplate = `
+global:
+  resolve_timeout: 5m
+  http_config:
+    follow_redirects: false
+    enable_http2: false
+route:
+  receiver: test
+  routes:
+    - receiver: test
+receivers:
+  - name: test
+    %s:
+      - send_resolved: false
+        routing_key: test
+        to: test
+        webhook_url_file: test
+`
