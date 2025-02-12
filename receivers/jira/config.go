0fc7267765a24b95921a9e2066c96a84835729a7
Santiago <santiagohernandez.1997@gmail.com>
2025-02-12 16:10:29 +0100

    add token, update tests

--- receivers/jira/config.go
+++ receivers/jira/config.go
@@ -36,6 +36,7 @@ type Config struct {
 
 	User     string
 	Password string
+	Token    string
 }
 
 func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Config, error) {
@@ -54,6 +55,7 @@ func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Confi
 		Fields            map[string]any `yaml:"fields,omitempty" json:"fields,omitempty"`
 		User              string         `yaml:"username,omitempty" json:"username,omitempty"`
 		Password          string         `yaml:"password,omitempty" json:"password,omitempty"`
+		Token             string         `yaml:"api_token,omitempty" json:"api_token,omitempty"`
 	}
 
 	settings := raw{}
@@ -97,12 +99,12 @@ func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Confi
 
 	// https://developer.atlassian.com/cloud/jira/platform/basic-auth-for-rest-apis/#basic-auth-for-rest-apis
 	settings.User = decryptFn("username", settings.User)
-	if settings.User == "" {
-		return Config{}, errors.New("username not found")
-	}
 	settings.Password = decryptFn("password", settings.Password)
-	if settings.Password == "" {
-		return Config{}, errors.New("password not found")
+	settings.Token = decryptFn("api_token", settings.Token)
+	if settings.Token == "" {
+		if settings.User == "" || settings.Password == "" {
+			return Config{}, errors.New("either username and password or api_token must be provided")
+		}
 	}
 
 	return Config{
@@ -120,5 +122,6 @@ func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Confi
 		Fields:            settings.Fields,
 		User:              settings.User,
 		Password:          settings.Password,
+		Token:             settings.Token,
 	}, nil
 }
--- receivers/jira/config_test.go
+++ receivers/jira/config_test.go
@@ -2,17 +2,23 @@ package jira
 
 import (
 	"encoding/json"
+	"net/url"
 	"testing"
 	"time"
 
+	receiversTesting "github.com/grafana/alerting/receivers/testing"
 	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewConfig(t *testing.T) {
+	testURL, err := url.Parse("http://localhost")
+	require.NoError(t, err)
+
 	cases := []struct {
 		name              string
 		settings          string
+		secureSettings    map[string][]byte
 		expectedConfig    Config
 		expectedInitError string
 	}{
@@ -29,33 +35,41 @@ func TestNewConfig(t *testing.T) {
 		{
 			name:     "Minimal valid configuration",
 			settings: `{"api_url": "http://localhost", "project": "test", "issue_type": "test"}`,
+			secureSettings: map[string][]byte{
+				"api_token": []byte("test-token"),
+			},
 			expectedConfig: Config{
-				URL:         "http://localhost",
+				URL:         testURL,
 				Project:     "test",
 				IssueType:   "test",
 				Summary:     `{{ template "jira.default.summary" . }}`,
 				Description: `{{ template "jira.default.description" . }}`,
 				Priority:    `{{ template "jira.default.priority" . }}`,
+				Token:       "test-token",
 			},
 		},
 		{
 			name: "Extracts all fields",
 			settings: `{
-				"api_url":           "http://localhost",
-				"project":           "Test Project",
-				"summary":           "Test Summary",
-				"description":       "Test Description",
-				"labels":            ["Test Label"],
-				"priority":          "Test Priority",
-				"issue_type":         "Test Issue Type",
-				"reopen_transition":  "Test Reopen Transition",
-				"resolve_transition": "Test Resolve Transition",
-				"wont_fix_resolution": "Test Won't Fix Resolution",
-				"reopen_duration":    "1m",
-				"fields":            {"test-field": "test-value"}
+				"api_url":           	"http://localhost",
+				"project":           	"Test Project",
+				"summary":           	"Test Summary",
+				"description":       	"Test Description",
+				"labels":            	["Test Label"],
+				"priority":          	"Test Priority",
+				"issue_type":         	"Test Issue Type",
+				"reopen_transition":  	"Test Reopen Transition",
+				"resolve_transition": 	"Test Resolve Transition",
+				"wont_fix_resolution":	"Test Won't Fix Resolution",
+				"reopen_duration":    	"1m",
+				"fields":            	{"test-field": "test-value"}
 			}`,
+			secureSettings: map[string][]byte{
+				"password": []byte("test-password"),
+				"username": []byte("test-username"),
+			},
 			expectedConfig: Config{
-				URL:               "http://localhost",
+				URL:               testURL,
 				Project:           "Test Project",
 				Summary:           "Test Summary",
 				Description:       "Test Description",
@@ -67,13 +81,15 @@ func TestNewConfig(t *testing.T) {
 				WontFixResolution: "Test Won't Fix Resolution",
 				ReopenDuration:    model.Duration(1 * time.Minute),
 				Fields:            map[string]any{"test-field": "test-value"},
+				User:              "test-username",
+				Password:          "test-password",
 			},
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			actual, err := NewConfig(json.RawMessage(c.settings))
+			actual, err := NewConfig(json.RawMessage(c.settings), receiversTesting.DecryptForTesting(c.secureSettings))
 
 			if c.expectedInitError != "" {
 				require.ErrorContains(t, err, c.expectedInitError)
