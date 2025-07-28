c6f18f35b6316847ce2cf3bca49cf707267e2f3a
William Wernert <william.wernert@gmail.com>
2025-07-28 09:52:39 -0400

    Disable keepalives in NewTLSClient (#359)
    This client is only used in short lived contexts so we don't want to
    keep the connections it opens alive since they can't be reused.

--- receivers/util.go
+++ receivers/util.go
@@ -87,6 +87,31 @@ func (cfg *TLSConfig) ToCryptoTLSConfig() (*tls.Config, error) {
 	return tlsCfg, nil
 }
 
+// NewTLSClient creates a new HTTP client with the provided TLS configuration or with default settings.
+func NewTLSClient(tlsConfig *tls.Config) *http.Client {
+	nc := func(tlsConfig *tls.Config) *http.Client {
+		return &http.Client{
+			Timeout: time.Second * 30,
+			Transport: &http.Transport{
+				TLSClientConfig: tlsConfig,
+				Proxy:           http.ProxyFromEnvironment,
+				Dial: (&net.Dialer{
+					Timeout: 30 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+				// Disable keep alive since this is always used as a short lived client
+				DisableKeepAlives: true,
+			},
+		}
+	}
+
+	if tlsConfig == nil {
+		return nc(&tls.Config{Renegotiation: tls.RenegotiateFreelyAsClient})
+	}
+
+	return nc(tlsConfig)
+}
+
 // SendHTTPRequest sends an HTTP request.
 // Stubbable by tests.
 //
