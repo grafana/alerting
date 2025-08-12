fd624521d550a27a24be338814a169739a406207
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-08-12 10:47:14 -0400

    Add dump function to hash package (#366)

--- utils/hash/hash.go
+++ utils/hash/hash.go
@@ -18,7 +18,13 @@ var configForHash = &spew.ConfigState{
 // DeepHashObject writes specified object to hash using the spew library
 // which follows pointers and prints actual values of the nested objects
 // ensuring the hash does not change when a pointer changes.
-func DeepHashObject(hasher hash.Hash, objectToWrite interface{}) {
+func DeepHashObject(hasher hash.Hash, objectToWrite any) {
 	hasher.Reset()
 	configForHash.Fprintf(hasher, "%#v", objectToWrite)
+}
+
+// Dump serializes any object into a detailed string representation using a predefined configuration that is used for calculating the hash.
+// Useful for debugging hash calculation
+func Dump(objectToWrite any) string {
+	return configForHash.Sprintf("%#v", objectToWrite)
 }
