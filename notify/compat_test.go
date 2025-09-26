26931d7138b86bd3985e8e54150157cf23609067
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-09-26 12:34:36 -0400

    move compats from Grafana

--- notify/compat_test.go
+++ notify/compat_test.go
@@ -8,34 +8,130 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/alerting/definition"
+	"github.com/grafana/alerting/models"
 )
 
-func TestPostableApiReceiverToApiReceiver(t *testing.T) {
-	postableReceiver := &definition.PostableApiReceiver{
-		Receiver: config.Receiver{
-			Name: "test",
-		},
-		PostableGrafanaReceivers: definition.PostableGrafanaReceivers{
-			GrafanaManagedReceivers: []*definition.PostableGrafanaReceiver{{
-				UID:                   "abc",
-				Name:                  "test",
-				Type:                  "slack",
-				DisableResolveMessage: true,
-				Settings:              definition.RawMessage{'b', 'y', 't', 'e', 's'},
-				SecureSettings:        map[string]string{"key": "value"},
-			}},
+func TestPostableAPIReceiverToAPIReceiver(t *testing.T) {
+	t.Run("returns empty when no receivers", func(t *testing.T) {
+		r := &definition.PostableApiReceiver{
+			Receiver: config.Receiver{
+				Name: "test-receiver",
+			},
+		}
+		actual := PostableAPIReceiverToAPIReceiver(r)
+		require.Empty(t, actual.Integrations)
+		require.Equal(t, r.Receiver, actual.ConfigReceiver)
+	})
+	t.Run("converts receivers", func(t *testing.T) {
+		r := &definition.PostableApiReceiver{
+			Receiver: config.Receiver{
+				Name: "test-receiver",
+			},
+			PostableGrafanaReceivers: definition.PostableGrafanaReceivers{
+				GrafanaManagedReceivers: []*definition.PostableGrafanaReceiver{
+					{
+						UID:                   "test-uid",
+						Name:                  "test-name",
+						Type:                  "slack",
+						DisableResolveMessage: false,
+						Settings:              definition.RawMessage(`{ "data" : "test" }`),
+						SecureSettings: map[string]string{
+							"test": "data",
+						},
+					},
+					{
+						UID:                   "test-uid2",
+						Name:                  "test-name2",
+						Type:                  "webhook",
+						DisableResolveMessage: false,
+						Settings:              definition.RawMessage(`{ "data2" : "test2" }`),
+						SecureSettings: map[string]string{
+							"test2": "data2",
+						},
+					},
+				},
+			},
+		}
+		actual := PostableAPIReceiverToAPIReceiver(r)
+		require.Len(t, actual.Integrations, 2)
+		require.Equal(t, r.Receiver, actual.ConfigReceiver)
+		require.Equal(t, *PostableGrafanaReceiverToIntegrationConfig(r.GrafanaManagedReceivers[0]), *actual.Integrations[0])
+		require.Equal(t, *PostableGrafanaReceiverToIntegrationConfig(r.GrafanaManagedReceivers[1]), *actual.Integrations[1])
+	})
+}
+
+func TestPostableGrafanaReceiverToGrafanaIntegrationConfig(t *testing.T) {
+	r := &definition.PostableGrafanaReceiver{
+		UID:                   "test-uid",
+		Name:                  "test-name",
+		Type:                  "slack",
+		DisableResolveMessage: false,
+		Settings:              definition.RawMessage(`{ "data" : "test" }`),
+		SecureSettings: map[string]string{
+			"test": "data",
 		},
 	}
-	receiver := PostableAPIReceiverToAPIReceiver(postableReceiver)
+	actual := PostableGrafanaReceiverToIntegrationConfig(r)
+	require.Equal(t, models.IntegrationConfig{
+		UID:                   "test-uid",
+		Name:                  "test-name",
+		Type:                  "slack",
+		DisableResolveMessage: false,
+		Settings:              json.RawMessage(`{ "data" : "test" }`),
+		SecureSettings: map[string]string{
+			"test": "data",
+		},
+	}, *actual)
+}
 
-	require.Equal(t, "test", receiver.Name)
-	require.Equal(t, 1, len(receiver.ReceiverConfig.Integrations))
+func TestPostableApiAlertingConfigToApiReceivers(t *testing.T) {
+	t.Run("returns empty when no receivers", func(t *testing.T) {
+		actual := PostableAPIReceiversToAPIReceivers(nil)
+		require.Empty(t, actual)
+	})
+	receivers := []*definition.PostableApiReceiver{
+		{
+			Receiver: config.Receiver{
+				Name: "test-receiver",
+			},
+			PostableGrafanaReceivers: definition.PostableGrafanaReceivers{
+				GrafanaManagedReceivers: []*definition.PostableGrafanaReceiver{
+					{
+						UID:                   "test-uid",
+						Name:                  "test-name",
+						Type:                  "slack",
+						DisableResolveMessage: false,
+						Settings:              definition.RawMessage(`{ "data" : "test" }`),
+						SecureSettings: map[string]string{
+							"test": "data",
+						},
+					},
+				},
+			},
+		},
+		{
+			Receiver: config.Receiver{
+				Name: "test-receiver2",
+			},
+			PostableGrafanaReceivers: definition.PostableGrafanaReceivers{
+				GrafanaManagedReceivers: []*definition.PostableGrafanaReceiver{
+					{
+						UID:                   "test-uid2",
+						Name:                  "test-name1",
+						Type:                  "slack",
+						DisableResolveMessage: false,
+						Settings:              definition.RawMessage(`{ "data" : "test" }`),
+						SecureSettings: map[string]string{
+							"test": "data",
+						},
+					},
+				},
+			},
+		},
+	}
+	actual := PostableAPIReceiversToAPIReceivers(receivers)
 
-	i := receiver.ReceiverConfig.Integrations[0]
-	require.Equal(t, "abc", i.UID)
-	require.Equal(t, "test", i.Name)
-	require.Equal(t, "slack", i.Type)
-	require.Equal(t, true, i.DisableResolveMessage)
-	require.Equal(t, json.RawMessage{'b', 'y', 't', 'e', 's'}, i.Settings)
-	require.Equal(t, map[string]string{"key": "value"}, i.SecureSettings)
+	require.Len(t, actual, 2)
+	require.Equal(t, PostableAPIReceiverToAPIReceiver(receivers[0]), actual[0])
+	require.Equal(t, PostableAPIReceiverToAPIReceiver(receivers[1]), actual[1])
 }
