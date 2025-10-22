92156cf5edfcdac4ad5b54c95d0dcd03f701382d
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2025-10-22 15:01:20 -0400

    make TestIntegration return IntegrationStatus

--- notify/receivers_testing.go
+++ notify/receivers_testing.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-openapi/strfmt"
 	"github.com/prometheus/alertmanager/config"
 	"github.com/prometheus/alertmanager/notify"
 	"github.com/prometheus/alertmanager/types"
+	"github.com/prometheus/common/model"
 
 	"github.com/grafana/alerting/models"
 	"github.com/grafana/alerting/notify/nfstatus"
 )
 
-func (am *GrafanaAlertmanager) TestIntegration(ctx context.Context, receiverName string, integrationConfig models.IntegrationConfig, alert models.TestReceiversConfigAlertParams) error {
+func (am *GrafanaAlertmanager) TestIntegration(
+	ctx context.Context,
+	receiverName string,
+	integrationConfig models.IntegrationConfig,
+	alert models.TestReceiversConfigAlertParams,
+) (models.IntegrationStatus, error) {
 	am.reloadConfigMtx.RLock()
 	templates := am.templates
 	am.reloadConfigMtx.RUnlock()
@@ -27,7 +34,7 @@ func TestIntegration(ctx context.Context,
 	testAlert models.TestReceiversConfigAlertParams,
 	buildIntegrationsFunc func(*APIReceiver, TemplatesProvider) ([]*nfstatus.Integration, error),
 	tmplProvider TemplatesProvider,
-) error {
+) (models.IntegrationStatus, error) {
 	nf, err := buildIntegrationsFunc(&APIReceiver{
 		ConfigReceiver: config.Receiver{
 			Name: receiverName,
@@ -37,10 +44,20 @@ func TestIntegration(ctx context.Context,
 		},
 	}, tmplProvider)
 	if err != nil || len(nf) == 0 {
-		return err
+		return models.IntegrationStatus{}, err
 	}
 	now := time.Now()
-	return TestNotifier(ctx, nf[0], newTestAlert(&testAlert, now, now), now)
+	err = TestNotifier(ctx, nf[0], newTestAlert(&testAlert, now, now), now)
+	result := models.IntegrationStatus{
+		LastNotifyAttempt:         strfmt.DateTime(now),
+		LastNotifyAttemptDuration: model.Duration(time.Since(now)).String(),
+		Name:                      nf[0].Name(),
+		SendResolved:              nf[0].SendResolved(),
+	}
+	if err != nil {
+		result.LastNotifyAttemptError = err.Error()
+	}
+	return result, nil
 }
 
 func TestNotifier(ctx context.Context, notifier *nfstatus.Integration, testAlert types.Alert, now time.Time) error {
