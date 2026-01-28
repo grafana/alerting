246355bae1342e0ee4e069b9a674336327131886
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2026-01-28 15:18:10 -0500

    copy HTTPClientConfig and add compat functions

---
+++ http/v0mimir1/client.go
@@ -0,0 +1,15 @@
+package v0mimir1
+
+import (
+	"net/http"
+
+	commoncfg "github.com/prometheus/common/config"
+)
+
+func NewClientFromConfig(cfg HTTPClientConfig, name string, optFuncs ...commoncfg.HTTPClientOption) (*http.Client, error) {
+	converted := cfg.ToCommonHTTPClientConfig()
+	if err := converted.Validate(); err != nil {
+		return nil, err
+	}
+	return commoncfg.NewClientFromConfig(*converted, name, optFuncs...)
+}
