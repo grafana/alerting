045e700babd8886cdb105c52987b24621789649e
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2026-02-25 16:37:02 -0500

    rollback valid config changes

--- notify/notifytest/mimir_integrations_test.go
+++ notify/notifytest/mimir_integrations_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestConfigIdempotency(t *testing.T) {
 	for iType := range AllValidMimirConfigs {
-		t.Run(iType.Name(), func(t *testing.T) {
+		t.Run(iType.PkgPath(), func(t *testing.T) {
 			cfg, err := GetRawConfigForMimirIntegration(iType, WithDefault)
 			require.NoError(t, err)
 			cfgInstance, err := GetMimirIntegrationForType(iType, WithDefault)
